e6e695093c39c5b255d9668a16465a0a74bbd259
AshiqueAhammed <ashique.ahamed9@gamil.com>
2020-10-14 17:04:31 +0530

    Validation of First name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 echo "Welcome to User Registration Program"
+
+function validateFirstName()
+{
+	echo "Enter Your first name"
+	read firstname
+	pattern="^[A-Z]{1}[a-z]{2}$"
+	if [[ $firstname =~ $pattern ]]
+	then
+		echo "Name is valid"
+	else
+		echo "Name is invalid"
+	fi
+}
+main()
+{
+validateFirstName
+}
+main
